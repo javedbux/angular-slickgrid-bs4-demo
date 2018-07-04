b2590d11722d8b90649b62788615e754a83a0443
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-07-04 19:40:30 -0400

    initial commit - full working Bootstrap 4 demo

---
+++ src/app/examples/grid-additem.component.ts
@@ -0,0 +1,157 @@
+import { Component, OnInit, Injectable } from '@angular/core';
+import { AngularGridInstance, Column, Editors, FieldType, Formatters, GridOption, OnEventArgs } from 'angular-slickgrid';
+
+@Component({
+  templateUrl: './grid-additem.component.html'
+})
+@Injectable()
+export class GridAddItemComponent implements OnInit {
+  title = 'Example 11: Add / Update / Highlight a Datagrid Item';
+  subTitle = `
+  Add / Update / Hightlight an Item from the Datagrid (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Add,-Update-or-Highlight-a-Datagrid-Item" target="_blank">Wiki docs</a>).
+  <ul>
+    <li><b>Note:</b> this demo is <b>only</b> on the datagrid (client) side, you still need to deal with the backend yourself</li>
+    <li>Adding an item, will always be showing as the 1st item in the grid because that is the best visual place to add it</li>
+    <li>Add/Update an item requires a valid Slickgrid Selection Model, you have 2 choices to deal with this:</li>
+    <ul><li>You can enable "enableCheckboxSelector" or "enableRowSelection" to True</li></ul>
+    <li>Click on any of the buttons below to test this out</li>
+    <li>You can change the highlighted color &amp; animation by changing the SASS variables:</li>
+    <ul>
+      <li>"$row-highlight-background-color" or "$row-highlight-fade-animation"</li>
+      <li>Take a look at the available <a href="https://github.com/ghiscoding/Angular-Slickgrid/blob/master/src/app/modules/angular-slickgrid/styles/_variables.scss" target="_blank">SASS Variables</a></li>
+    </ul>
+  </ul>
+  `;
+
+  angularGrid: AngularGridInstance;
+  columnDefinitions: Column[];
+  gridOptions: GridOption;
+  dataset: any[];
+  updatedObject: any;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.columnDefinitions = [
+      {
+        id: 'title', name: 'Title', field: 'title',
+        sortable: true,
+        type: FieldType.string,
+        editor: {
+          model: Editors.longText
+        }
+      },
+      {
+        id: 'duration', name: 'Duration (days)', field: 'duration',
+        sortable: true,
+        type: FieldType.number,
+        editor: {
+          model: Editors.text
+        },
+        onCellChange: (e: Event, args: OnEventArgs) => {
+          alert('onCellChange directly attached to the column definition');
+          console.log(args);
+        }
+      },
+      {
+        id: 'complete', name: '% Complete', field: 'percentComplete',
+        formatter: Formatters.percentCompleteBar,
+        type: FieldType.number,
+        editor: {
+          model: Editors.integer
+        }
+      },
+      {
+        id: 'start', name: 'Start', field: 'start',
+        formatter: Formatters.dateIso,
+        sortable: true,
+        type: FieldType.date,
+        /*
+        editor: {
+          model: Editors.date
+        }
+        */
+      },
+      {
+        id: 'finish', name: 'Finish', field: 'finish',
+        formatter: Formatters.dateIso, sortable: true,
+        type: FieldType.date
+      },
+      {
+        id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven',
+        formatter: Formatters.checkmark,
+        type: FieldType.number,
+        editor: {
+          model: Editors.checkbox
+        }
+      }
+    ];
+
+    this.gridOptions = {
+      asyncEditorLoading: false,
+      autoResize: {
+        containerId: 'demo-container',
+        sidePadding: 15
+      },
+      editable: true,
+      enableColumnPicker: true,
+      enableCellNavigation: true,
+      enableRowSelection: true
+    };
+
+    // mock a dataset
+    const mockedDataset = [];
+    for (let i = 0; i < 1000; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor((Math.random() * 29));
+      const randomPercent = Math.round(Math.random() * 100);
+
+      mockedDataset[i] = {
+        id: i,
+        title: 'Task ' + i,
+        duration: Math.round(Math.random() * 100) + '',
+        percentComplete: randomPercent,
+        percentCompleteNumber: randomPercent,
+        start: new Date(randomYear, randomMonth, randomDay),
+        finish: new Date(randomYear, (randomMonth + 1), randomDay),
+        effortDriven: (i % 5 === 0)
+      };
+    }
+    this.dataset = mockedDataset;
+  }
+
+  addNewItem() {
+    const newId = this.dataset.length;
+    const randomYear = 2000 + Math.floor(Math.random() * 10);
+    const randomMonth = Math.floor(Math.random() * 11);
+    const randomDay = Math.floor((Math.random() * 29));
+    const randomPercent = Math.round(Math.random() * 100);
+
+    const newItem = {
+      id: newId,
+      title: 'Task ' + newId,
+      duration: Math.round(Math.random() * 100) + '',
+      percentComplete: randomPercent,
+      percentCompleteNumber: randomPercent,
+      start: new Date(randomYear, randomMonth, randomDay),
+      finish: new Date(randomYear, (randomMonth + 2), randomDay),
+      effortDriven: true
+    };
+    this.angularGrid.gridService.addItemToDatagrid(newItem);
+  }
+
+  highlighFifthRow() {
+    this.angularGrid.gridService.highlightRow(4, 1500);
+  }
+
+  angularGridReady(angularGrid: any) {
+    this.angularGrid = angularGrid;
+  }
+
+  updateSecondItem() {
+    const firstItem = this.angularGrid.gridService.getDataItemByRowNumber(1);
+    firstItem.duration = Math.round(Math.random() * 100);
+    this.angularGrid.gridService.updateDataGridItem(firstItem);
+  }
+}
---
+++ src/app/examples/grid-rowselection.component.ts
@@ -0,0 +1,129 @@
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AngularGridInstance, Column, FieldType, Formatters, GridOption } from 'angular-slickgrid';
+
+@Component({
+  templateUrl: './grid-rowselection.component.html'
+})
+@Injectable()
+export class GridRowSelectionComponent implements OnInit {
+  title = 'Example 10: Multiple Grids with Row Selection';
+  subTitle = `
+    Row selection, single or multi-select (<a href="https://github.com/ghiscoding/Angular-Slickgrid/wiki/Row-Selection" target="_blank">Wiki docs</a>).
+    <ul>
+      <li>Single Select, you can click on any cell to make the row active</li>
+      <li>Multiple Selections, you need to specifically click on the checkbox to make 1 or more selections</li>
+      <li>Note that "enableExcelCopyBuffer" cannot be used at the same time as Row Selection because there can exist only 1 SelectionModel at a time</li>
+    </ul>
+  `;
+
+  angularGrid1: AngularGridInstance;
+  angularGrid2: AngularGridInstance;
+  columnDefinitions1: Column[];
+  columnDefinitions2: Column[];
+  gridOptions1: GridOption;
+  gridOptions2: GridOption;
+  dataset1: any[];
+  dataset2: any[];
+  gridObj1: any;
+  gridObj2: any;
+  selectedTitles: any[];
+  selectedTitle: any;
+
+  ngOnInit(): void {
+    this.prepareGrid();
+  }
+
+  angularGridReady1(angularGrid: AngularGridInstance) {
+    this.angularGrid1 = angularGrid;
+    this.gridObj1 = angularGrid && angularGrid.slickGrid || {};
+  }
+
+  angularGridReady2(angularGrid: AngularGridInstance) {
+    this.angularGrid2 = angularGrid;
+    this.gridObj2 = angularGrid && angularGrid.slickGrid || {};
+  }
+
+  prepareGrid() {
+    this.columnDefinitions1 = [
+      {id: 'title', name: 'Title', field: 'title', sortable: true, type: FieldType.string},
+      {id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true, type: FieldType.number},
+      {id: 'complete', name: '% Complete', field: 'percentComplete', formatter: Formatters.percentCompleteBar, type: FieldType.number, sortable: true},
+      {id: 'start', name: 'Start', field: 'start', formatter: Formatters.dateIso, sortable: true, type: FieldType.dateIso, exportWithFormatter: true },
+      {id: 'finish', name: 'Finish', field: 'finish', formatter: Formatters.dateIso, sortable: true, type: FieldType.date, exportWithFormatter: true },
+      {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', formatter: Formatters.checkmark, type: FieldType.number, sortable: true}
+    ];
+    this.columnDefinitions2 = [
+      {id: 'title', name: 'Title', field: 'title', sortable: true, type: FieldType.string},
+      {id: 'duration', name: 'Duration (days)', field: 'duration', sortable: true, type: FieldType.number},
+      {id: 'complete', name: '% Complete', field: 'percentComplete', formatter: Formatters.percentCompleteBar, type: FieldType.number, sortable: true},
+      {id: 'start', name: 'Start', field: 'start', formatter: Formatters.dateIso, sortable: true, type: FieldType.dateIso, exportWithFormatter: true },
+      {id: 'finish', name: 'Finish', field: 'finish', formatter: Formatters.dateIso, sortable: true, type: FieldType.date, exportWithFormatter: true },
+      {id: 'effort-driven', name: 'Effort Driven', field: 'effortDriven', formatter: Formatters.checkmark, type: FieldType.number, sortable: true}
+    ];
+    this.gridOptions1 = {
+      enableAutoResize: false,
+      enableCellNavigation: true,
+      enableCheckboxSelector: true,
+      rowSelectionOptions: {
+        // True (Single Selection), False (Multiple Selections)
+        selectActiveRow: true
+      },
+      enableRowSelection: true
+    };
+    this.gridOptions2 = {
+      enableAutoResize: false,
+      enableCellNavigation: true,
+      rowSelectionOptions: {
+        // True (Single Selection), False (Multiple Selections)
+        selectActiveRow: false
+      },
+      preselectedRows: [0, 2],
+      enableCheckboxSelector: true,
+      enableRowSelection: true,
+    };
+
+    this.dataset1 = this.prepareData();
+    this.dataset2 = this.prepareData();
+  }
+
+  prepareData() {
+    // mock a dataset
+    const mockDataset = [];
+    for (let i = 0; i < 500; i++) {
+      const randomYear = 2000 + Math.floor(Math.random() * 10);
+      const randomMonth = Math.floor(Math.random() * 11);
+      const randomDay = Math.floor((Math.random() * 29));
+      const randomPercent = Math.round(Math.random() * 100);
+
+      mockDataset[i] = {
+        id: i,
+        title: 'Task ' + i,
+        duration: Math.round(Math.random() * 100) + '',
+        percentComplete: randomPercent,
+        percentCompleteNumber: randomPercent,
+        start: new Date(randomYear, randomMonth, randomDay),
+        finish: new Date(randomYear, (randomMonth + 1), randomDay),
+        effortDriven: (i % 5 === 0)
+      };
+    }
+    return mockDataset;
+  }
+
+  handleSelectedRowsChanged1(e, args) {
+    if (Array.isArray(args.rows)) {
+      this.selectedTitle = args.rows.map(idx => {
+        const item = this.gridObj1.getDataItem(idx);
+        return item.title || '';
+      });
+    }
+  }
+
+  handleSelectedRowsChanged2(e, args) {
+    if (Array.isArray(args.rows)) {
+      this.selectedTitles = args.rows.map(idx => {
+        const item = this.gridObj2.getDataItem(idx);
+        return item.title || '';
+      });
+    }
+  }
+}
